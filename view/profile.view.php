b147aa7c2d1cade0a84b19d9003b3e0d745ff55f
Nimitae <nimitae@hotmail.com>
2014-05-29 19:02:17 -0700

    Introduced tabbing for profile page

--- view/profile.view.php
+++ view/profile.view.php
@@ -6,6 +6,25 @@
 </head>
 <body role="document">
 <?php include('partials/navbar.partial.view.php') ?>
+<div class="container" role="main">
+    <h3>Profile Management</h3>
+
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#home" data-toggle="tab">Home</a></li>
+        <li><a href="#profile" data-toggle="tab">Profile</a></li>
+        <li><a href="#messages" data-toggle="tab">Messages</a></li>
+        <li><a href="#settings" data-toggle="tab">Settings</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div class="tab-pane active" id="home">...</div>
+        <div class="tab-pane" id="profile">...</div>
+        <div class="tab-pane" id="messages">...</div>
+        <div class="tab-pane" id="settings">...</div>
+    </div>
+</div>
+
+
 
 <?php include('partials/footer.partial.view.php') ?>
 </body>
