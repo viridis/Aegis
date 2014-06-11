69a44a07661dc5237568d276646ea8788b79c6c3
Nimitae <nimitae@hotmail.com>
2014-06-11 11:27:29 +0800

    Refactored participate service and included authorisation checks when processing event sign ups

--- service/participate.service.php
+++ service/participate.service.php
@@ -65,7 +65,7 @@ class ParticipateService
         $character = $dataService->getCharacterByCharID($takenCharID);
         /** @var Slot $slot */
         $slot = $dataService->getSlotBySlotID($slotID);
-        if (!$this->isValidPairing($character, $slot)) {
+        if (!$this->isValidSignUp($character, $slot)) {
             return false;
         }
         $slot->setTaken(true);
@@ -79,30 +79,67 @@ class ParticipateService
         return true;
     }
 
-    private function isValidPairing($character, $slot)
+    private function isValidSignUp($character, $slot)
     {
         /** @var Slot $slot */
         /** @var Character $character */
         $dataService = new DataService();
         /** @var Event $event */
         $event = $dataService->getEventByEventID($slot->getEventID());
+        if (!$this->userHasRightsToCharacter($character)){
+            return false;
+        }
+        if ($this->characterOnCooldownForEvent($character, $event)){
+            return false;
+        }
+        if (!$this->characterCanFitSlot($character, $slot, $dataService)){
+            return false;
+        }
+        return true;
+    }
 
-        // Check for cooldown for eventType
+    private function userHasRightsToCharacter($character)
+    {
+        /** @var Character $character */
+        $dataService = new DataService();
+        /** @var User $charUser */
+        $charUser = $dataService->getUserByUserID($character->getUserID());
+        /** @var User $sessionUser */
+        $sessionUser = $dataService->getUserByUserID($_SESSION["userID"]);
+        if (($charUser->getUserID() != $sessionUser->getUserID()) && ($sessionUser->getRoleLevel() < 10)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private function characterOnCooldownForEvent($character, $event)
+    {
+        /** @var Event $event */
+        /** @var Character $character */
         $eventDate = strtotime($event->getStartDate());
         $cooldownDate = 0;
         if (isset($character->getCooldownContainer()[$event->getEventTypeID()])) {
             $cooldownDate = strtotime($character->getCooldownContainer()[$event->getEventTypeID()]);
         }
-        if ($cooldownDate > $eventDate){
+        if ($cooldownDate > $eventDate) {
+            return true;
+        } else {
             return false;
         }
+    }
 
-        // Check for slotClass restriction
+    private function characterCanFitSlot($character, $slot)
+    {
+        $dataService = new DataService();
+        /** @var Slot $slot */
+        /** @var Character $character */
         /** @var SlotClass $slotClass */
         $slotClass = $dataService->getSlotClassBySlotClassID($slot->getSlotClassID());
-        if (!isset($slotClass->getAllowedCharClassArray()[$character->getCharClassID()])){
+        if (isset($slotClass->getAllowedCharClassArray()[$character->getCharClassID()])) {
+            return true;
+        } else {
             return false;
         }
-        return true;
     }
 }
