5b295bee0f3eb3259cf556229284e5b65e16fcd3
Nimitae <nimitae@hotmail.com>
2014-06-19 12:19:56 +0800

    View implemented for management of users.

--- controller/users.php
+++ controller/users.php
@@ -1,70 +1,24 @@
 <?php
 require_once("../service/page.service.php");
-require_once("../service/user.service.php");
+require_once("../service/data.service.php");
+require_once("../service/manageUsers.service.php");
 
 if (!$_SESSION["userID"]) {
     header("location: ./home.php");
 }
 $pageservice = new PageService();
+$dataService = new DataService();
 $currentPageID = "Manage Users";
 if (isset($_SESSION["userID"])) {
     $sessionUser = $pageservice->whoIsSessionUser($_SESSION["userID"]);
-    $navbarlinks = $pageservice->generateNavLinksForUser($_SESSION["userID"]);
+    $navBarLinks = $pageservice->generateNavLinksForUser($_SESSION["userID"]);
 } else {
-    $navbarlinks = $pageservice->generateNavLinksForUser();
+    $navBarLinks = $pageservice->generateNavLinksForUser();
 }
-$usefulllinks = $pageservice->generateUsefulLinks(5);
-$featuredlinks = $pageservice->generateFeaturedLinks(5);
+$usefulLinks = $pageservice->generateUsefulLinks(5);
+$featuredLinks = $pageservice->generateFeaturedLinks(5);
 
-$userservice = new UserService();
-if (isset($_POST["addUser"])) {
-    try {
-        $user = $userservice->createUser($_POST["addUser"]);
-        $notification = array(
-            'type' => 'success',
-            'title' => 'Success',
-            'message' => 'Successfully added user. (' . $user->getName() . ')',
-        );
-    } catch (Exception $e) {
-        $notification = array(
-            'type' => 'error',
-            'message' => $e->getMessage(),
-        );
-    }
-}
 
-if (isset($_GET['editUser']) && is_numeric($_GET['editUser'])) {
-    try {
-        $user = $userservice->getUserByUserID($_GET['editUser']);
-        $result['action'] = 'requestUser';
-        $result['user']['id'] = $user->getId();
-        $result['user']['name'] = $user->getName();
-        $result['user']['mailName'] = $user->getMailName();
-        print(json_encode($result));
-        exit();
-    } catch (Exception $e) {
-        print($e);
-        exit();
-    }
-}
 
-if (isset($_POST['editUserID']) && isset($_POST['editUserName']) && isset($_POST['editUserMailName']) && is_numeric($_POST['editUserID'])) {
-    try {
-        $user = $userservice->updateUser($_POST['editUserID'], $_POST['editUserName'], $_POST['editUserMailName']);
-        $notification = array(
-            'type' => 'success',
-            'title' => 'Success',
-            'message' => 'Successfully changed user. (' . $user->getName() . ')',
-        );
-    } catch (Exception $e) {
-        $notification = array(
-            'type' => 'danger',
-            'title' => 'Error',
-            'message' => $e->getMessage(),
-        );
-    }
-}
-
-$userlist = $userservice->getAllUsers();
+$userContainer  = $dataService->getAllUserInfo();
 include("../view/users.view.php");
-?>
--- view/users.view.php
+++ view/users.view.php
@@ -8,179 +8,81 @@
 <body>
 <?php include('partials/navbar.partial.view.php') ?>
 <div class="container" role="main">
-
-    <div class="modal fade" id="addUser">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Add a user</h4>
+    <h3>Manage Users</h3>
+    Note: click to edit, enter to save
+    <table class="table table-bordered" id="eventTemplateTable">
+        <thead>
+        <tr>
+            <th>Username</th>
+            <th>Role Level</th>
+            <th>GMT</th>
+            <th>Email</th>
+            <th>Mail Char</th>
+            <th>Password</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach($userContainer as $user) :
+            /** @var User $user */ ?>
+            <tr>
+                <td><?php print $user->getUserLogin();?></td>
+                <td class="edit" id="roleLevel_<?php print $user->getUserID();?>"><?php print $user->getRoleLevel();?></td>
+                <td class="edit" id="GMT_<?php print $user->getUserID();?>"><?php print $user->getGMT(); ?></td>
+                <td class="edit" id="email_<?php print $user->getUserID();?>"><?php print $user->getEmail(); ?></td>
+                <td class="edit" id="mailChar<?php print $user->getUserID();?>"><?php print $user->getMailChar(); ?></td>
+                <td class="edit" id="password_<?php print $user->getUserID();?>">Click to Change</td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+    <div class="container col-sm-6">
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label for="newUser" class="control-label col-sm-4">New User</label>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control" id="newUser" name="newUser"
+                           placeholder="User Login">
                 </div>
-                <form action="users.php" method="post">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="addUser" name="addUser" placeholder="Username">
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <input type="submit" class="btn btn-primary" value="Add User">
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <div class="modal fade" id="editUser">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Edit a user</h4>
+                <div class="col-sm-2">
+                    <input type="button" class="btn btn-primary" value="Create"
+                           id="createUser" onclick="createUser(); return false;">
                 </div>
-                <form id="submitEditUser" action="users.php" method="post">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <input type="hidden" class="form-control" id="editUserID" name="editUserID"
-                                   placeholder="User ID">
-                            <label class="control-label">Username: </label>
-                            <input type="text" class="form-control" id="editUserName" name="editUserName"
-                                   placeholder="User Name">
-                            <label class="control-label">Mailname: </label>
-                            <input type="text" class="form-control" id="editUserMailName" name="editUserMailName"
-                                   placeholder="User Mail Name">
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <input type="submit" class="btn btn-primary" value="Edit User">
-                    </div>
-                </form>
             </div>
-        </div>
+        </form>
     </div>
-
-    <?php if (isset($notification)): ?>
-        <div class="alert alert-<?php print($notification['type']); ?> alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <p><strong><?php print($notification['title']); ?></strong> - <?php print($notification['message']); ?></p>
-        </div>
-    <?php endif; ?>
-
-    <div style="height: 20px;"></div>
-
-    <div class="pull-right btn-group-vertical">
-        <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#addUser">
-            Add User
-        </button>
-    </div>
-
-    <div class="container">
-        <div class="row center">
-            <div class="col-sm-4 col-md-offset-4">
-                <table class="table table-condensed table-hover table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nickname</th>
-                        <th>Mailname</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <?php
-                    $i = 0;
-                    foreach ($userContainer as $user) {
-                        $i++;
-                        ?>
-                        <tr>
-                            <td><?php print($i); ?></td>
-                            <td><?php print($user->getName()); ?></td>
-                            <td><?php print($user->getMailName()); ?></td>
-                            <td>
-                                <span class="glyphicon glyphicon-pencil hand"
-                                      onclick="editUser(<?php print($user->getId()); ?>);" data-toggle="modal"
-                                      data-target="#editUser"></span>
-                            </td>
-                        </tr>
-                    <?php
-                    }
-                    ?>
-                </table>
-            </div>
-        </div>
-    </div>
-
 </div>
 
 
+
 <?php include('partials/footer.partial.view.php') ?>
+
 <script>
-    window.onload = function () {
-        document.getElementById("addUserButton").onclick = function (event) {
-            popup('popAddUser', 'block');
-            setFocus('addUser');
-        }
-        document.getElementById("popupWrapper").onclick = function (event) {
-            popup('popAddUser', 'none');
-            popup('popEditUser', 'none');
-        }
+    $(document).ready(function () {
+        $('.edit').editable('manageEventTemplate.php', {
+            id: 'id',
+            name: 'value'
+        });
+    });
 
-    }
-    window.onresize = initializePage;
-
-    //close popup boxes using ESC key.
-    document.onkeydown = function (event) {
-        event = event || window.event;
-        if (event.keyCode == 27) {
-            popup('popAddUser', 'none');
-        }
+    function createUser() {
+        var newUserLogin = $('#newUser').val();
+        makeAddAjaxRequest(newUserLogin);
     }
 
-    function editUser(id) {
-        makeRequest("users.php?editUser=" + id);
+    function makeAddAjaxRequest(newUserLogin) {
+
+        $.ajax({
+            type: "POST",
+            data: { newUserLogin: newUserLogin},
+            url: "users.php",
+            success: function (result) {
+                processJSONUpdateTable(result);
+            }
+        });
     }
 
-    function serverResponse(httpRequest) {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                result = JSON.parse(httpRequest.responseText);
-                if (result['action'] == "requestUser") {
-                    document.getElementById("editUserID").value = result['user']['id'];
-                    document.getElementById("editUserName").value = result['user']['name'];
-                    document.getElementById("editUserMailName").value = result['user']['mailName'];
-                }
-            }
-            else {
-                alert('There was a problem with the request.');
-            }
-        }
-    }
-
-    function makeRequest(url) {
-        var httpRequest;
-        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-            httpRequest = new XMLHttpRequest();
-        }
-        else if (window.ActiveXObject) { // IE
-            try {
-                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-            }
-            catch (e) {
-                try {
-                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-                }
-                catch (e) {
-                }
-            }
-        }
-
-        if (!httpRequest) {
-            alert('Giving up :( Cannot create an XMLHTTP instance');
-            return false;
-        }
-        httpRequest.onreadystatechange = function () {
-            serverResponse(httpRequest);
-        };
-        httpRequest.open('POST', url, true);
-        httpRequest.send();
+    function processJSONUpdateTable(result) {
+        $('#eventTemplateTable tr:last').after(result);
     }
 </script>
 </body>
